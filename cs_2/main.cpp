dbcb22a185421e37ebee01010c3f493f0a6d5fca
slukjanov <me@frostman.ru>
2011-04-18 13:15:25 +0400

    cs_4: implemented

--- cs_2/main.cpp
+++ cs_2/main.cpp
@@ -3,6 +3,7 @@
 #include <conio.h>
 #include <string>
 #include <windows.h>
+#include <sstream>
 
 #define __DEBUG__OFF
 
@@ -12,13 +13,9 @@
 
 int main(int argc, char * argv[]) {	
 	std::string file_path = argv[0];
-
-	printf("Before read data. Touch me quikly asd asd asd asd.\n");
-	sign_data_t read_data = calc_sign_data(file_path);
-	printf("After read data. Touch me quikly asd asd asd asd.\n");
-
-	sign_data_t curr_data = calc_sign_data(file_path);
 	
+	sign_data_t read_data = read_sign_data(file_path);
+	sign_data_t curr_data = calc_sign_data(file_path);
 	
 	if(!read_data.ready) {
 		printf("First run. Appending sign into the file.\n");		
@@ -29,29 +26,40 @@ int main(int argc, char * argv[]) {
 	} else {
 		printf("Comparing...\n");
 
+		std::stringstream out;
 		bool changed = false;
 		if(read_data.dir != curr_data.dir) {
-			printf("Directory changed\n\t(from '%s' to '%s')\n", read_data.dir.c_str(), curr_data.dir.c_str());
+			out << "Directory changed\n\t(from '" << read_data.dir
+				<< "' to '"<< curr_data.dir
+				<< "')\n";
 			changed = true;
 		}
 
 		if(read_data.name != curr_data.name) {
-			printf("File name changed\n\t(from '%s' to '%s')\n", read_data.name.c_str(), curr_data.name.c_str());
+			out << "File name changed\n\t(from '" << read_data.name
+				<< "' to '" << curr_data.name
+				<< "')\n";
 			changed = true;
 		}
 
 		if(read_data.hash_code != curr_data.hash_code) {
-			printf("Hash code changed\n\t(from '%s' to '%s')\n", read_data.hash_code.c_str(), curr_data.hash_code.c_str());
+			out << "Hash code changed\n\t(from '" << read_data.hash_code
+				<< "' to '" << curr_data.hash_code
+				<< "')\n";
 			changed = true;
 		}
 
 		if(read_data.file_attrs != curr_data.file_attrs) {
-			printf("File attributes changed\n\t(from '%s' to '%s')\n", read_data.file_attrs.c_str(), curr_data.file_attrs.c_str());
+			out << "File attributes changed\n\t(from '" << read_data.file_attrs
+				<< "' to '" << curr_data.file_attrs
+				<< "')\n";
 			changed = true;
 		}
 
 		if(!changed) {
 			printf("Nothing changed.\n");
+		} else {
+			printf("Changes:\n%s", out.str().c_str());
 		}
 	}	
 
--- cs_4/main.cpp
+++ cs_4/main.cpp
@@ -1,9 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <conio.h>
 
-int main() {
+using namespace std;
 
-	printf("ok");
+int main(int argc, char * argv[]) {
+	if(argc != 2) {
+		printf("Please specify path to .exe file\n");
+		_getch();
+		return 1;
+	}
+
+	std::string path = argv[1];	
+	
+	HANDLE hRead = CreateFile(path.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, 0);
+
+	if(hRead == INVALID_HANDLE_VALUE) {
+		printf("Can't open file: %s", path);
+		_getch();
+		return -1;
+	}
+	
+	DWORD dwFileSize = GetFileSize(hRead, NULL);
+	PBYTE FileBuffer = (PBYTE) malloc(dwFileSize * sizeof(BYTE));
+	DWORD dwAmountRead;
+	
+	BOOL res = ReadFile(hRead, (PVOID)FileBuffer, dwFileSize, &dwAmountRead, NULL);
+	CloseHandle(hRead);
+	if(!res) {
+		printf("Failed to read file\n");
+		_getch();
+		return -1;
+	}
+	
+	for(int i = 0x1902; i <= 0x1903; ++i){
+		FileBuffer[i] = ((BYTE) 0x90);
+	}
+	HANDLE hWrite = CreateFile(path.c_str(), GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
+	WriteFile(hWrite, FileBuffer, dwFileSize, &dwAmountRead, NULL);
+	CloseHandle(hWrite);
+	printf("File '%s' successfully cracked.\n", path.c_str());
+	_getch();
 
 	return 0;
 }
